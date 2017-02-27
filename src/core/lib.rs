40804006c2051b2b95ecc077f5bfeb1ec5c99cc5
Jo Liss <joliss42@gmail.com>
2017-02-27 17:31:00 +0000

    More progress on serialization

--- src/core/lib.rs
+++ src/core/lib.rs
@@ -1,9 +1,25 @@
 #![allow(dead_code)]
 
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate serde_derive;
 
 extern crate serde_json;
+extern crate serde;
+extern crate toml;
+extern crate semver_parser;
+
+use std::collections::HashMap;
+
+macro_rules! map(
+    { $($key:expr => $value:expr),+ } => {
+        {
+            let mut m = ::std::collections::HashMap::new();
+            $(
+                m.insert($key, $value);
+            )+
+            m
+        }
+     };
+);
 
 pub fn test() {
     let r = registry::Repository {
@@ -11,8 +27,37 @@ pub fn test() {
         url: "https://...".to_string() };
     println!("r = {:?}", r);
     println!("json = {}", serde_json::to_string(&r).unwrap());
+    println!("\ntoml:\n{}", toml::to_string(&r).unwrap());
     let deserialized: registry::Repository = serde_json::from_str("{\"repository_type\":\"git\",\"url\":\"https://...\"}").unwrap();
     println!("deserialized = {:?}", deserialized);
+
+    // TODO: implement VersionConstraint serialization
+    println!("version_constraint:\n{}",
+        toml::to_string(&registry::VersionConstraint::Exact(registry::Version {
+            fields: vec![1, 0, 0],
+            prerelease: vec![],
+            build: vec![],
+        })).unwrap());
+
+    // let reg = registry::Registry {
+    //     packages: map! {
+    //         registry::PackageName { namespace: "rust".to_string(), name: "foo".to_string() }
+    //           => registry::Package { owners: vec![], releases: HashMap::new() }
+    //     }
+    // };
+    // println!("reg: {}", toml::to_string(&reg).unwrap());
+
+    let test_registry_toml = r#"
+[packages."rust/foo"]
+owners = ["bodil"]
+
+[packages."rust/foo".releases."1.0.0"]
+dependencies = [
+  { name = "rust/bar", version_constraint = "" }
+]
+    "#;
+    let test_registry: registry::Registry = toml::from_str(test_registry_toml).unwrap();
+    println!("test_registry: {:?}", test_registry)
 }
 
 pub mod registry;
--- src/core/registry.rs
+++ src/core/registry.rs
@@ -1,17 +1,24 @@
 use std::collections::HashMap;
+use std::string::String;
+use std::iter::Iterator;
+use std::fmt::Display;
+use serde::{Serialize, Serializer, Deserialize, Deserializer};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Registry {
     pub packages: HashMap<PackageName, Package>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Package {
     pub releases: HashMap<Version, Release>,
     pub owners: Vec<Username>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Release {
     pub dependencies: Vec<Dependency>,
     // no devDependencies here -- they only go in the manifest
@@ -30,12 +37,14 @@ pub struct Release {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Dependency {
     pub name: PackageName,
     pub version_constraint: VersionConstraint,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub enum VersionConstraint {
     Exact(Version),
     Range { // exclusive
@@ -44,26 +53,100 @@ pub enum VersionConstraint {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct PackageName {
     pub namespace: String,
     pub name: String
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
+impl Display for PackageName {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
+        write!(f, "{}/{}", self.namespace, self.name)
+    }
+}
+
+impl Serialize for PackageName {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(&*self.to_string())
+    }
+}
+
+impl Deserialize for PackageName {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer
+    {
+        let s = String::deserialize(deserializer)?;
+        let v: Vec<&str> = s.split('/').collect();
+        Ok(PackageName { namespace: v[0].to_string(), name: v[1].to_string() })
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Version {
     pub fields: Vec<u64>,
     pub prerelease: Vec<VersionIdentifier>,
-    pub build: Vec<VersionIdentifier>
+    pub build: Vec<VersionIdentifier> // TODO Vec?
+}
+
+impl Serialize for Version {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        let mut s = String::new();
+        s.push_str(&*self.fields.iter().map(|f| f.to_string()).collect::<Vec<_>>().join("."));
+        if self.prerelease.len() > 0 {
+            s.push_str("-");
+            s.push_str(&*self.prerelease.iter().map(|f| f.to_string()).collect::<Vec<_>>().join("."));
+        }
+        if self.build.len() > 0 {
+            s.push_str("+");
+            s.push_str(&*self.build.iter().map(|f| f.to_string()).collect::<Vec<_>>().join("."));
+        }
+        serializer.serialize_str(&*s)
+    }
+}
+
+impl Deserialize for Version {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer
+    {
+        let s = String::deserialize(deserializer)?;
+        let v = ::semver_parser::version::parse(&*s).unwrap();
+        fn cast_identifier(i: &::semver_parser::version::Identifier) -> VersionIdentifier {
+            match *i {
+                ::semver_parser::version::Identifier::Numeric(ref n) => VersionIdentifier::Numeric(n.clone()),
+                ::semver_parser::version::Identifier::AlphaNumeric(ref s) => VersionIdentifier::AlphaNumeric(s.clone())
+            }
+        }
+        Ok(Version {
+            fields: vec![v.major, v.minor, v.patch],
+            prerelease: v.pre.iter().map(cast_identifier).collect(),
+            build: v.build.iter().map(cast_identifier).collect(),
+        })
+
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
+#[serde(deny_unknown_fields)]
 pub enum VersionIdentifier {
     Numeric(u64),
     AlphaNumeric(String)
 }
 
+impl Display for VersionIdentifier {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
+        match *self {
+            VersionIdentifier::Numeric(ref n) => write!(f, "{}", n),
+            VersionIdentifier::AlphaNumeric(ref s) => write!(f, "{}", s)
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Repository {
     pub repository_type: String,
     pub url: String,
