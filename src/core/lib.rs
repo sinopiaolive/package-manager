dca1931c875176f21d32343982aa8fd6bd8737f5
Jo Liss and Bodil Stokke <joliss42+bodil.stokke@gmail.com>
2017-02-28 18:28:31 +0000

    Finish parsing of version strings.

--- src/core/lib.rs
+++ src/core/lib.rs
@@ -6,6 +6,7 @@ extern crate serde;
 #[macro_use] extern crate serde_derive;
 extern crate serde_json;
 extern crate toml;
+extern crate regex;
 
 pub mod registry;
 pub mod version;
--- src/core/version.rs
+++ src/core/version.rs
@@ -1,19 +1,39 @@
 use registry::{Version, VersionIdentifier};
 use registry::VersionIdentifier::{Numeric, Alphanumeric};
-use nom::{digit, alphanumeric, IResult, ErrorKind};
+use nom::{digit, ErrorKind};
+#[allow(unused_imports)] use nom::IResult::Done;
 use std::str::FromStr;
 use std::str;
+use regex::Regex;
 
-named!(nat<u64>, map_res!(map_res!(digit, str::from_utf8), u64::from_str));
-
-named!(numeric<VersionIdentifier>, map!(nat, |n| Numeric(n)));
+named!(nat<u64>, map_res!(map_res!(digit, str::from_utf8), to_u64));
 
 named!(base_version<Vec<u64>>, separated_nonempty_list!(char!('.'), nat));
 
-// TODO are any non-alphanumss allowed?
-named!(alpha<VersionIdentifier>, map!(map!(map_res!(alphanumeric, str::from_utf8), str::to_string), |n| Alphanumeric(n)));
 
-named!(version_id<VersionIdentifier>, alt!(numeric | alpha));
+named!(version_id<VersionIdentifier>, map_res!(map_res!(re_bytes_find!(r"^[a-zA-Z0-9-]+"), str::from_utf8), convert_version_identifier));
+
+fn to_u64(s: &str) -> Result<u64, ()> {
+    match u64::from_str(s) {
+        Ok(n) => {
+            if s.chars().count() > 1 && s.chars().next() == Some('0') {
+                Err(())
+            } else {
+                Ok(n)
+            }
+        }
+        Err(_) => Err(())
+    }
+}
+
+fn convert_version_identifier(s: &str) -> Result<VersionIdentifier, ()> {
+    let numeric_version_id_re: Regex = Regex::new(r"^[0-9]+$").unwrap();
+    if numeric_version_id_re.is_match(s) {
+        to_u64(s).map(Numeric)
+    } else {
+        Ok(Alphanumeric(s.to_string()))
+    }
+}
 
 named!(ext_version<Vec<VersionIdentifier>>, separated_nonempty_list!(char!('.'), version_id));
 
@@ -41,29 +61,45 @@ named!(version<Version>, do_parse!(
 
 #[test]
 fn parse_nat() {
-    assert_eq!(nat(b"1"), IResult::Done(&b""[..], 1));
-    assert_eq!(nat(b"123"), IResult::Done(&b""[..], 123));
+    assert_eq!(nat(b"1"), Done(&b""[..], 1));
+    assert_eq!(nat(b"123"), Done(&b""[..], 123));
     assert!(nat(b"123456789123456789123456789123456789").is_err());
-    assert_eq!(nat(b"123lol"), IResult::Done(&b"lol"[..], 123));
+    assert_eq!(nat(b"123lol"), Done(&b"lol"[..], 123));
     assert!(nat(b"wat").is_err());
 }
 
 #[test]
+fn parse_version_identifier() {
+    assert_eq!(version_id(b"1"), Done(&b""[..], Numeric(1)));
+    assert_eq!(version_id(b"-1"), Done(&b""[..], Alphanumeric("-1".to_string())));
+    assert_eq!(version_id(b"1a-"), Done(&b""[..], Alphanumeric("1a-".to_string())));
+    assert_eq!(version_id(b"1a-_"), Done(&b"_"[..], Alphanumeric("1a-".to_string())));
+    assert!(version_id(b"00").is_err());
+    assert!(version_id(b"01").is_err());
+    assert!(version_id(b"123456789123456789123456789123456789").is_err());
+}
+
+#[test]
 fn parse_base_version() {
-    assert_eq!(base_version(b"1"), IResult::Done(&b""[..], vec![1]));
-    assert_eq!(base_version(b"1.2.3"), IResult::Done(&b""[..], vec![1, 2, 3]));
-    assert_eq!(base_version(b"1.2.3.4.5"), IResult::Done(&b""[..], vec![1, 2, 3, 4, 5]));
+    assert_eq!(base_version(b"1"), Done(&b""[..], vec![1]));
+    assert_eq!(base_version(b"1.2.3"), Done(&b""[..], vec![1, 2, 3]));
+    assert_eq!(base_version(b"1.2.3.4.5"), Done(&b""[..], vec![1, 2, 3, 4, 5]));
 }
 
 #[test]
 fn parse_full_version() {
-    assert_eq!(version(b"1"), IResult::Done(&b""[..], Version::new(vec![1], vec![], vec![])));
-    assert_eq!(version(b"1.2.3"), IResult::Done(&b""[..], Version::new(vec![1, 2, 3], vec![], vec![])));
-    assert_eq!(version(b"1-2+3"), IResult::Done(&b""[..], Version::new(vec![1], vec![Numeric(2)], vec![Numeric(3)])));
-    assert_eq!(version(b"1+3"), IResult::Done(&b""[..], Version::new(vec![1], vec![], vec![Numeric(3)])));
-    assert_eq!(version(b"1-2"), IResult::Done(&b""[..], Version::new(vec![1], vec![Numeric(2)], vec![])));
-    assert_eq!(version(b"1.2-2.foo+bar.3"), IResult::Done(&b""[..], Version::new(vec![1, 2], vec![Numeric(2), Alphanumeric("foo".to_string())], vec![Alphanumeric("bar".to_string()), Numeric(3)])));
-    assert_eq!(version(b"1.2+3.4.lol"), IResult::Done(&b""[..], Version::new(vec![1, 2], vec![], vec![Numeric(3), Numeric(4), Alphanumeric("lol".to_string())])));
-    assert_eq!(version(b"1.3-omg.2"), IResult::Done(&b""[..], Version::new(vec![1, 3], vec![Alphanumeric("omg".to_string()), Numeric(2)], vec![])));
-    //assert_eq!(version(b"1-123456789123456789123456789123456789"), IResult::Done(&b""[..], Version::new(vec![1], vec![], vec![])));
+    assert_eq!(version(b"1"), Done(&b""[..], Version::new(vec![1], vec![], vec![])));
+    assert_eq!(version(b"1.2.3"), Done(&b""[..], Version::new(vec![1, 2, 3], vec![], vec![])));
+    assert_eq!(version(b"1-2+3"), Done(&b""[..], Version::new(vec![1], vec![Numeric(2)], vec![Numeric(3)])));
+    assert_eq!(version(b"1+3"), Done(&b""[..], Version::new(vec![1], vec![], vec![Numeric(3)])));
+    assert_eq!(version(b"1-2"), Done(&b""[..], Version::new(vec![1], vec![Numeric(2)], vec![])));
+    assert_eq!(version(b"1.2-2.foo+bar.3"), Done(&b""[..], Version::new(vec![1, 2], vec![Numeric(2), Alphanumeric("foo".to_string())], vec![Alphanumeric("bar".to_string()), Numeric(3)])));
+    assert_eq!(version(b"1.2+3.4.lol"), Done(&b""[..], Version::new(vec![1, 2], vec![], vec![Numeric(3), Numeric(4), Alphanumeric("lol".to_string())])));
+    assert_eq!(version(b"1.3-omg.2"), Done(&b""[..], Version::new(vec![1, 3], vec![Alphanumeric("omg".to_string()), Numeric(2)], vec![])));
+    assert!(version(b"1-123456789123456789123456789123456789").is_err());
+    assert!(version(b"1-01").is_err());
+    assert!(version(b"1-_not_alphanumeric_").is_err());
+    assert!(version(b"1+123456789123456789123456789123456789").is_err());
+    assert!(version(b"1+01").is_err());
+    assert!(version(b"1+_not_alphanumeric_").is_err());
 }
