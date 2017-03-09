14871e0441a46e061fc8f0881e9b423c12b16bf7
Bodil Stokke <bodil@bodil.org>
2017-03-09 15:13:19 +0000

    Registry ordering needs to be preserved.

--- src/core/registry.rs
+++ src/core/registry.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use linked_hash_map::LinkedHashMap;
 use std::string::String;
 use version::Version;
 use manifest::{PackageName, Manifest};
@@ -6,22 +6,23 @@ use manifest::{PackageName, Manifest};
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct Registry {
-    pub packages: HashMap<PackageName, Package>,
+    pub packages: LinkedHashMap<PackageName, Package>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct Package {
-    pub releases: HashMap<Version, Release>,
+    pub releases: LinkedHashMap<Version, Release>,
     pub owners: Vec<Username>,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-#[serde(default)] // we're using this for testing; can get rid of it later
+#[serde(default)]
+// we're using this for testing; can get rid of it later
 #[serde(deny_unknown_fields)]
 pub struct Release {
     pub artifact_url: String,
-    pub manifest: Manifest
+    pub manifest: Manifest,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
