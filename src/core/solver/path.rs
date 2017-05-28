9cc13d0e29e92f19782e2a1ed89798e16aca101e
Jo Liss <joliss42@gmail.com>
2017-05-28 01:05:18 +0000

    Document that Paths go right-to-left.

--- src/core/solver/path.rs
+++ src/core/solver/path.rs
@@ -3,4 +3,14 @@ use list::List;
 use manifest::PackageName;
 use version::Version;
 
+/// A dependency chain of packages.
+///
+/// Note that the dependency graph flows right-to-left, so that new dependencies
+/// are cons'ed onto the beginning. That is, the following dependency chain:
+///
+///     A 1 -> B 1 -> C 1
+///
+/// is stored as the following Path object:
+///
+///     [(C, 1), (B, 1), (A, 1)]
 pub type Path = List<(Arc<PackageName>, Arc<Version>)>;
