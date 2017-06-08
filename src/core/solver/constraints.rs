971ca120144c6044aa8f5411c40441615aed0ffd
Bodil Stokke <bodil@bodil.org>
2017-06-08 16:23:10 +0100

    Newtype constraint types and move merge functions into ConstraintSet

--- src/core/solver/constraints.rs
+++ src/core/solver/constraints.rs
@@ -1,10 +1,49 @@
 use manifest::PackageName;
 use solver::path::Path;
-use immutable_map::map::TreeMap as Map;
+use immutable_map::map::{TreeMap as Map, TreeMapIter};
 use std::sync::Arc;
 use version::Version;
+use solver::solution::{PartialSolution, JustifiedVersion};
+use solver::failure::Failure;
+use std::iter::IntoIterator;
 
-pub type Constraint = Map<Arc<Version>, Path>;
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Constraint(Map<Arc<Version>, Path>);
+
+impl Constraint {
+    pub fn new() -> Constraint {
+        Constraint(Map::new())
+    }
+
+    pub fn iter<'r>(&'r self) -> TreeMapIter<'r, Arc<Version>, Path> {
+        self.0.iter()
+    }
+
+    pub fn insert(&self, key: Arc<Version>, value: Path) -> Constraint {
+        Constraint(self.0.insert(key, value))
+    }
+
+    pub fn get(&self, key: &Version) -> Option<&Path> {
+        self.0.get(key)
+    }
+
+    pub fn contains_key(&self, key: &Version) -> bool {
+        self.0.contains_key(key)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl<'r> IntoIterator for &'r Constraint {
+    type Item = (&'r Arc<Version>, &'r Path);
+    type IntoIter = TreeMapIter<'r, Arc<Version>, Path>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
 
 // impl fmt::Debug for Constraint {
 //     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
@@ -12,18 +51,65 @@ pub type Constraint = Map<Arc<Version>, Path>;
 //     }
 // }
 
-pub type ConstraintSet = Map<Arc<PackageName>, Constraint>;
+#[derive(Clone, PartialEq, Eq)]
+pub struct ConstraintSet(Map<Arc<PackageName>, Constraint>);
 
-// impl ConstraintSet {
-//     pub fn pop(&self) -> (Option<(Arc<PackageName>, Constraint)>, ConstraintSet) {
-//         // FIXME obvs
-//         (None, ConstraintSet(Map::new()))
-//     }
-//     pub fn add(&self, path: &Path, facts: DependencySet) -> ConstraintSet {
-//         // FIXME obvs
-//         ConstraintSet(Map::new())
-//     }
-// }
+impl ConstraintSet {
+    pub fn new() -> ConstraintSet {
+        ConstraintSet(Map::new())
+    }
+
+    pub fn pop(&self) -> Option<(ConstraintSet, (Arc<PackageName>, Constraint))> {
+        match self.0.delete_min() {
+            None => None,
+            Some((new_set, (k, v))) =>
+                Some((ConstraintSet(new_set), (k.clone(), v.clone())))
+        }
+    }
+
+    pub fn insert(&self, key: Arc<PackageName>, value: Constraint) -> ConstraintSet {
+        ConstraintSet(self.0.insert(key, value))
+    }
+
+    pub fn get(&self, key: &PackageName) -> Option<&Constraint> {
+        self.0.get(key)
+    }
+
+    pub fn contains_key(&self, key: &PackageName) -> bool {
+        self.0.contains_key(key)
+    }
+
+    pub fn merge(&self,
+                 new: &ConstraintSet,
+                 solution: &PartialSolution)
+                 -> Result<ConstraintSet, Failure> {
+        let mut out = self.clone();
+        for (package, new_constraint) in new {
+            if contained_in(package.clone(), new_constraint, solution)? {
+                continue;
+            }
+            out = match out.get(package) {
+                None => out.insert(package.clone(), new_constraint.clone()),
+                Some(ref existing_constraint) => {
+                    out.insert(package.clone(),
+                               merge_constraints(package.clone(),
+                                                 &existing_constraint,
+                                                 &new_constraint)?)
+                }
+            }
+        }
+        Ok(out)
+    }
+}
+
+impl<'r> IntoIterator for &'r ConstraintSet {
+    type Item = (&'r Arc<PackageName>, &'r Constraint);
+    type IntoIter = TreeMapIter<'r, Arc<PackageName>, Constraint>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
 
 // impl fmt::Debug for Constraints {
 //     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
@@ -38,3 +124,43 @@ pub type ConstraintSet = Map<Arc<PackageName>, Constraint>;
 //         }
 //     }
 // }
+
+fn contained_in(package: Arc<PackageName>,
+                constraint: &Constraint,
+                solution: &PartialSolution)
+                -> Result<bool, Failure> {
+    match solution.get(&package.clone()) {
+        None => Ok(false),
+        Some(&JustifiedVersion {
+            ref version,
+            ref path,
+        }) if !constraint.contains_key(&version.clone()) => {
+            let exact_constraint = Constraint::new().insert(version.clone(), path.clone());
+            Err(Failure::conflict(package.clone(), exact_constraint, constraint.clone()))
+        }
+        _ => Ok(true),
+    }
+}
+
+fn merge_constraints(package: Arc<PackageName>,
+                     a: &Constraint,
+                     b: &Constraint)
+                     -> Result<Constraint, Failure> {
+    let mut out = Constraint::new();
+    for (version, a_path) in a {
+        if let Some(ref b_path) = b.get(version) {
+            let shortest_path = if a_path.length() <= b_path.length() {
+                a_path
+            } else {
+                b_path
+            };
+            out = out.insert(version.clone(), shortest_path.clone());
+        }
+    }
+
+    if out.is_empty() {
+        Err(Failure::conflict(package.clone(), a.clone(), b.clone()))
+    } else {
+        Ok(out)
+    }
+}
--- src/core/solver/mod.rs
+++ src/core/solver/mod.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use registry::Registry;
-use manifest::{PackageName, DependencySet};
+use manifest::DependencySet;
 #[cfg(test)] use test;
 
 mod path;
@@ -10,8 +10,8 @@ mod failure;
 mod solution;
 mod adapter;
 
-use solver::path::Path;
-use solver::constraints::{Constraint, ConstraintSet};
+use solver::constraints::ConstraintSet;
+#[cfg(test)] use solver::constraints::Constraint;
 use solver::failure::Failure;
 use solver::solution::{PartialSolution, Solution, JustifiedVersion};
 use solver::adapter::RegistryAdapter;
@@ -22,18 +22,18 @@ fn search(ra: Arc<RegistryAdapter>,
           solution: &PartialSolution)
           -> Result<PartialSolution, Failure> {
     // TODO replace .delete_min with a smarter strategy
-    match stack.delete_min() {
+    match stack.pop() {
         None => Ok(solution.clone()),
         Some((stack_tail, (package, constraint))) => {
             let mut first_failure: Option<Failure> = None;
-            for (version, path) in constraint {
+            for (version, path) in &constraint {
                 let new_solution = solution.insert(package.clone(), JustifiedVersion {
                     version: version.clone(),
                     path: path.clone()
                 });
                 let search_try_version = || {
                     let constraint_set = ra.constraint_set_for(package.clone(), version.clone(), path.clone())?;
-                    let new_deps = merge(&stack_tail, &constraint_set, &new_solution)?;
+                    let new_deps = stack_tail.merge(&constraint_set, &new_solution)?;
                     Ok(search(ra.clone(), &new_deps, cheap, &new_solution)?)
                 };
                 if cheap {
@@ -53,68 +53,6 @@ fn search(ra: Arc<RegistryAdapter>,
             }
             Err(first_failure.expect("unreachable: constraint should never be empty"))
         }
-    }
-}
-
-fn merge(existing: &ConstraintSet,
-         new: &ConstraintSet,
-         solution: &PartialSolution)
-         -> Result<ConstraintSet, Failure> {
-    let mut out = existing.clone();
-    for (package, new_constraint) in new {
-        if contained_in(package.clone(), new_constraint, solution)? {
-            continue;
-        }
-        out = match out.get(package) {
-            None => out.insert(package.clone(), new_constraint.clone()),
-            Some(ref existing_constraint) => {
-                out.insert(package.clone(),
-                           merge_constraints(package.clone(),
-                                             &existing_constraint,
-                                             &new_constraint)?)
-            }
-        }
-    }
-    Ok(out)
-}
-
-fn contained_in(package: Arc<PackageName>,
-                constraint: &Constraint,
-                solution: &PartialSolution)
-                -> Result<bool, Failure> {
-    match solution.get(&package.clone()) {
-        None => Ok(false),
-        Some(&JustifiedVersion {
-                 ref version,
-                 ref path,
-             }) if !constraint.contains_key(&version.clone()) => {
-            let exact_constraint = Constraint::new().insert(version.clone(), path.clone());
-            Err(Failure::conflict(package.clone(), exact_constraint, constraint.clone()))
-        }
-        _ => Ok(true),
-    }
-}
-
-fn merge_constraints(package: Arc<PackageName>,
-                     a: &Constraint,
-                     b: &Constraint)
-                     -> Result<Constraint, Failure> {
-    let mut out = Constraint::new();
-    for (version, a_path) in a {
-        if let Some(ref b_path) = b.get(version) {
-            let shortest_path = if a_path.length() <= b_path.length() {
-                a_path
-            } else {
-                b_path
-            };
-            out = out.insert(version.clone(), shortest_path.clone());
-        }
-    }
-
-    if out.is_empty() {
-        Err(Failure::conflict(package.clone(), a.clone(), b.clone()))
-    } else {
-        Ok(out)
     }
 }
 
@@ -139,7 +77,7 @@ fn dependency_set_to_constraint_set(ra: Arc<RegistryAdapter>,
         let version_constraint_arc = Arc::new(version_constraint.clone());
         let constraint = ra.constraint_for(package_arc.clone(),
                                            version_constraint_arc.clone(),
-                                           Path::default())?;
+                                           list![])?;
         constraint_set = constraint_set.insert(package_arc.clone(), constraint);
     }
 
