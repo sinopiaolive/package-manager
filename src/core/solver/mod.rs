a87dd7ce04cb629786985f44f75f52194f84593e
Bodil Stokke <bodil@bodil.org>
2017-06-30 15:00:07 +0100

    Some tests over the Cargo dataset.

--- src/core/solver/mod.rs
+++ src/core/solver/mod.rs
@@ -164,11 +164,90 @@ fn infer_indirect_dependencies(
 #[cfg(test)]
 mod unit_test {
     use super::*;
-    use test::{pkg, ver, sample_registry};
+    use test::{pkg, ver, range, sample_registry};
     use solver::test::{constraint_set, partial_sln};
-
     use std::sync::Arc;
     use solver::constraints::Constraint;
+    use std::path;
+    use solver::error::{Error, Conflict};
+
+    #[test]
+    fn resolve_something_real() {
+        let reg = ::index::read_index(path::Path::new("test/cargo.rmp")).unwrap();
+
+        let problem = deps!{
+            tokio_proto => "^0",
+            hyper => "^0.11",
+            url => "^1"
+        };
+
+        assert_eq!(
+            solve(&reg, &problem),
+            Ok(solution!{
+                base64 => "0.6.0",
+                byteorder => "1.0.0",
+                bytes => "0.4.4",
+                cfg_if => "0.1.1",
+                futures => "0.1.14",
+                futures_cpupool => "0.1.5",
+                httparse => "1.2.3",
+                hyper => "0.11.0",
+                idna => "0.1.2",
+                iovec => "0.1.0",
+                kernel32_sys => "0.2.2",
+                language_tags => "0.2.2",
+                lazycell => "0.4.0",
+                libc => "0.2.24",
+                log => "0.3.8",
+                matches => "0.1.6",
+                mime => "0.3.2",
+                mio => "0.6.9",
+                miow => "0.2.1",
+                net2 => "0.2.29",
+                num_cpus => "1.6.2",
+                percent_encoding => "1.0.0",
+                rand => "0.3.15",
+                redox_syscall => "0.1.18",
+                safemem => "0.2.0",
+                scoped_tls => "0.1.0",
+                slab => "0.3.0",
+                smallvec => "0.2.1",
+                take => "0.1.0",
+                time => "0.1.37",
+                tokio_core => "0.1.8",
+                tokio_io => "0.1.2",
+                tokio_proto => "0.1.1",
+                tokio_service => "0.1.0",
+                unicase => "2.0.0",
+                unicode_bidi => "0.3.3",
+                unicode_normalization => "0.1.5",
+                url => "1.5.1",
+                winapi => "0.2.8",
+                ws2_32_sys => "0.2.1"
+            })
+        )
+    }
+
+    #[test]
+    fn deep_conflict() {
+        let reg = ::index::read_index(path::Path::new("test/cargo.rmp")).unwrap();
+
+        let problem = deps!{
+            rocket => "^0.2.8",
+            hyper_rustls => "^0.8"
+        };
+
+        assert_eq!(
+            solve(&reg, &problem),
+            Err(Error::Conflict(Conflict {
+                package: Arc::new(pkg("hyper")),
+                existing: range("^0.11"),
+                existing_path: conslist![(Arc::new(pkg("hyper_rustls")), Arc::new(ver("0.8.0")))],
+                conflicting: range("^0.10.4"),
+                conflicting_path: conslist![(Arc::new(pkg("rocket")), Arc::new(ver("0.2.8")))]
+            }))
+        )
+    }
 
     #[test]
     fn find_best_solution_set() {
