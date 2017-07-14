91d02bb883a148ddcf3bc8b82f60a37225df613a
Jo Liss <joliss42@gmail.com>
2017-07-14 11:13:56 +0000

    Run import-cargo against fixed registry state.

--- client/src/solver/mod.rs
+++ client/src/solver/mod.rs
@@ -217,19 +217,19 @@ mod unit_test {
                 solve(&reg, &problem),
                 Ok(solution!{
                     base64 => "0.6.0",
-                    byteorder => "1.0.0",
+                    byteorder => "1.1.0",
                     bytes => "0.4.4",
-                    cfg_if => "0.1.1",
+                    cfg_if => "0.1.2",
                     futures => "0.1.14",
                     futures_cpupool => "0.1.5",
                     httparse => "1.2.3",
-                    hyper => "0.11.0",
-                    idna => "0.1.2",
+                    hyper => "0.11.1",
+                    idna => "0.1.4",
                     iovec => "0.1.0",
                     kernel32_sys => "0.2.2",
                     language_tags => "0.2.2",
                     lazycell => "0.4.0",
-                    libc => "0.2.24",
+                    libc => "0.2.26",
                     log => "0.3.8",
                     matches => "0.1.6",
                     mime => "0.3.2",
@@ -239,19 +239,19 @@ mod unit_test {
                     num_cpus => "1.6.2",
                     percent_encoding => "1.0.0",
                     rand => "0.3.15",
-                    redox_syscall => "0.1.18",
+                    redox_syscall => "0.1.26",
                     safemem => "0.2.0",
                     scoped_tls => "0.1.0",
                     slab => "0.3.0",
                     smallvec => "0.2.1",
                     take => "0.1.0",
-                    time => "0.1.37",
+                    time => "0.1.38",
                     tokio_core => "0.1.8",
                     tokio_io => "0.1.2",
                     tokio_proto => "0.1.1",
                     tokio_service => "0.1.0",
                     unicase => "2.0.0",
-                    unicode_bidi => "0.3.3",
+                    unicode_bidi => "0.3.4",
                     unicode_normalization => "0.1.5",
                     url => "1.5.1",
                     winapi => "0.2.8",
@@ -279,7 +279,7 @@ mod unit_test {
                     existing: range("^0.11"),
                     existing_path: path(&[("hyper_rustls", "0.8.0")]),
                     conflicting: range("^0.10.4"),
-                    conflicting_path: path(&[("rocket", "0.2.8")]),
+                    conflicting_path: path(&[("rocket", "0.2.9")]),
                 }))
             );
         });
@@ -291,8 +291,7 @@ mod unit_test {
         let sample_reg = sample_registry();
         let sample_ra = RegistryAdapter::new(&sample_reg);
 
-        let problem =
-            deps!(
+        let problem = deps!(
             down_pad => "^1.0.0",
             left_pad => "^2.0.0"
         );
@@ -316,8 +315,7 @@ mod unit_test {
 
         // left_pad and lol_pad have conflicting constraints for right_pad,
         // thus no solution is possible.
-        let problem =
-            deps!(
+        let problem = deps!(
             left_pad => "^1.0.0",
             lol_pad => "^1.0.0"
         );
@@ -359,8 +357,7 @@ mod unit_test {
 
     #[test]
     fn infer_indirect_dependencies_test() {
-        let reg =
-            gen_registry!(
+        let reg = gen_registry!(
             X => (
                 "1" => deps!(
                     A => "1",
--- client/test/import-cargo/index.js
+++ client/test/import-cargo/index.js
@@ -1,10 +1,18 @@
 const path = require("path");
 const fs = require("fs");
+const childProcess = require("child_process");
 const glob = require("glob");
 const semver = require("semver");
 const msgpack = require("msgpack5")();
 
-const p = path.resolve(process.env.HOME, ".cargo", "registry", "index", "*", "*", "*", "*");
+const indexCommit = "cb69ec98b649c4f57e48cb34c086ef150910f16a";
+const indexPath = `crates.io-index-${indexCommit}`;
+
+if (!fs.existsSync(indexPath)) {
+  childProcess.execSync(`curl https://github.com/rust-lang/crates.io-index/archive/${indexCommit}.tar.gz | tar xz`);
+}
+
+const p = path.resolve(indexPath, "*", "*", "*");
 
 function normalisePkg(name) {
   return "test/" + name.replace("-", "_");
@@ -12,44 +20,55 @@ function normalisePkg(name) {
 
 function desugar(wat) {
   const r = wat.replace(",", " ");
+  // 1.2.x
   if (/^[0-9.]+[*xX]$/.exec(r)) {
     return semver.validRange(r);
   }
+  // 1.x.x
   if (/^[0-9.]+[*xX]\.[*xX]$/.exec(r)) {
     return semver.validRange(r);
   }
+  // = 1.2.3
   const m = /^= *([0-9a-zA-Z.-]+)$/.exec(r);
   if (m) {
     return m[1];
   }
+  // > 1.2.3
   const m2 = /^> *([0-9.]+)$/.exec(r);
   if (m2) {
     return `^${m2[1]}`;
   }
+  // ^1.2.3 >= 1.5.0
   const m3 = /^\^([0-9.]+),? *>= *([0-9.]+)$/.exec(r);
   if (m3) {
     return `^${m3[2]}`;
   }
+  // ^1.2.3 < 1.5.0
   const m4 = /^\^([0-9.]+),? *< *([0-9.]+)$/.exec(r);
   if (m4) {
     return `>= ${m4[1]} < ${m4[2]}`;
   }
+  // ^1.2.3 <= 1.5.0
   const m5 = /^\^([0-9.]+),? *<= *([0-9.]+)$/.exec(r);
   if (m5) {
     return `${m5[1]}`;
   }
+  // >= 1.2.3 <= 1.5.0
   const m6 = /^>= *([0-9.]+),? *<= *([0-9.]+)$/.exec(r);
   if (m6) {
     return `>= ${m6[1]} < ${m6[2]}`;
   }
+  // ^1.2.3 ^1.2.0
   const m7 = /^\^ *([0-9.]+),? *\^ *([0-9.]+)$/.exec(r);
   if (m7) {
     return `^${m7[1]}`;
   }
+  // > 1.2.3 < 1.5.0
   const m8 = /^> *([0-9.]+),? *< *([0-9.]+)$/.exec(r);
   if (m8) {
     return `>= ${m8[1]} < ${m8[2]}`;
   }
+  // >= 1.2.3 1.2.x
   const m9 = /^>= *([0-9.]+),? *([0-9.]+\.[*xX])$/.exec(r);
   if (m9) {
     return `^${m9[1]}`;
