24177c74ca99040deea9af9ee65c4e139b4aba66
Bodil Stokke <bodil@bodil.org>
2017-08-18 18:11:38 +0100

    Define the database schema manually.
    Requiring a live Postgres server to compile is unsettling, so let's
    not do that.

--- server/src/schema.rs
+++ server/src/schema.rs
@@ -1,1 +1,16 @@
-infer_schema!("dotenv:DATABASE_URL");
+table! {
+    users {
+        id -> VarChar,
+        name -> VarChar,
+        email -> VarChar,
+        avatar -> Nullable<VarChar>,
+    }
+}
+
+table! {
+    login_sessions (token) {
+        token -> VarChar,
+        callback -> VarChar,
+        stamp -> Timestamp,
+    }
+}
