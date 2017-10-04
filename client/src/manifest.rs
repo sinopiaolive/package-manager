eb062a6f54cc30aeaed8805c446c4ca6a3640154
Jo Liss <joliss42@gmail.com>
2017-10-04 16:23:20 +0000

    More progress on reading manifests.

--- client/src/manifest.rs
+++ client/src/manifest.rs
@@ -1,29 +1,27 @@
+use std::path::PathBuf;
 use pm_lib::manifest::{PackageName, DependencySet};
 use pm_lib::version::Version;
 
 // The Manifest struct represents a parsed manifest file.
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Manifest {
-    // Once we support groups, this will be a different type.
-    pub dependencies: DependencySet,
-
-    pub metadata: Option<Metadata>,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Metadata {
+pub struct Release {
     pub name: PackageName,
     pub version: Version,
 
+    pub dependencies: DependencySet,
+
+    pub authors: Vec<String>,
     pub description: Option<String>,
-    pub keywords: Vec<String>,
     pub homepage: Option<String>,
     pub bugs: Option<String>,
-    pub repository: Option<String>,
+    // We should infer the repository.
+    //pub repository: Option<String>,
+    pub keywords: Vec<String>,
 
     pub license: Option<String>,
-    pub license_files: Vec<String>,
+    pub license_files: Vec<PathBuf>,
 
-    // To do: files, authors
+    pub readme_contents: String,
+    pub files: Vec<PathBuf>,
 }
--- client/src/pest_parser.rs
+++ client/src/pest_parser.rs
@@ -8,7 +8,7 @@ use pm_lib::manifest::{PackageName, DependencySet};
 use pm_lib::version::{Version};
 use pm_lib::constraint::VersionConstraint;
 
-use manifest::{Metadata};
+use manifest::{Release};
 
 // Ensure this file recompiles when the grammar is modified.
 const _GRAMMAR: &'static str = include_str!("pest_grammar.pest");
@@ -22,13 +22,40 @@ pub type Error<'a> = pest::Error<pest_parser::Rule, pest::inputs::StrInput<'a>>;
 type Pair<'a> = pest::iterators::Pair<pest_parser::Rule, pest::inputs::StrInput<'a>>;
 type Pairs<'a> = pest::iterators::Pairs<pest_parser::Rule, pest::inputs::StrInput<'a>>;
 
-pub fn get_dependencies<'a>(manifest_source: &'a str)
-    -> Result<DependencySet, Error<'a>> {
+
+// Parse a manifest into a release.
+pub fn parse_into_release<'a>(manifest_source: &'a str)
+    -> Result<Release, Error<'a>>
+{
+    let manifest_pair = parse_manifest(manifest_source)?;
+
+    get_release(manifest_pair)
+}
+
+// Parse a manifest into just the dependencies. Faster than parse_into_release.
+pub fn parse_into_dependencies<'a>(manifest_source: &'a str)
+    -> Result<DependencySet, Error<'a>>
+{
+    let manifest_pair = parse_manifest(manifest_source)?;
+
+    get_dependencies(manifest_pair)
+}
+
+
+pub fn parse_manifest<'a>(manifest_source: &'a str)
+    -> Result<Pair<'a>, Error<'a>>
+{
     let pairs = ManifestParser::parse_str(Rule::manifest_eof, manifest_source)?;
 
     let manifest_eof_pair = find_in_pairs(pairs, Rule::manifest_eof);
     let manifest_pair = find(manifest_eof_pair, Rule::manifest);
-    let (maybe_dependencies_section_pair, maybe_metadata_section_pair) =
+    Ok(manifest_pair)
+}
+
+pub fn get_dependencies<'a>(manifest_pair: Pair<'a>)
+    -> Result<DependencySet, Error<'a>>
+{
+    let (maybe_dependencies_section_pair, _) =
         find_section_pairs(manifest_pair)?;
 
     let mut depset = DependencySet::new();
@@ -47,62 +74,92 @@ pub fn get_dependencies<'a>(manifest_source: &'a str)
             depset.insert(package_name, version_constraint);
         }
     }
-
-    let mut metadata: Option<Metadata> = None;
-    if let Some(metadata_section_pair) = maybe_metadata_section_pair {
-        let object_pair = find(metadata_section_pair, Rule::object);
-        check_object_fields(object_pair.clone(), &[
-            "name",
-            "version",
-
-            "description",
-            "keywords",
-            "homepage",
-            "bugs",
-            "repository",
-
-            "license",
-            "license_file",
-        ])?;
-
-        let name = {
-            let (name_pair, name_string)
-                = get_string(get_field(object_pair.clone(), "name")?)?;
-            PackageName::from_str(&name_string).ok_or_else(||
-                pest::Error::CustomErrorSpan {
-                    message: "Invalid package name".to_string(),
-                    span: name_pair.clone().into_span(),
-                }
-            )?
-        };
-
-        let version = {
-            let (version_pair, version_string)
-                = get_string(get_field(object_pair.clone(), "version")?)?;
-            Version::from_str(&version_string).ok_or_else(||
-                pest::Error::CustomErrorSpan {
-                    message: "Invalid version number".to_string(),
-                    span: version_pair.clone().into_span(),
-                }
-            )?
-        };
-
-        metadata = Some(Metadata {
-            name: name,
-            version: version,
-
-            description: None,
-            keywords: vec![],
-            homepage: None,
-            bugs: None,
-            repository: None,
-
-            license: None,
-            license_files: vec![],
-        });
-    }
-
     Ok(depset)
+}
+
+pub fn get_release<'a>(manifest_pair: Pair<'a>)
+    -> Result<Release, Error<'a>> {
+    let dependencies = get_dependencies(manifest_pair.clone())?;
+
+    let (maybe_dependencies_section_pair, maybe_metadata_section_pair) =
+        find_section_pairs(manifest_pair.clone())?;
+
+    let metadata_section_pair = maybe_metadata_section_pair.ok_or_else(||
+        pest::Error::CustomErrorPos {
+            message: "A `package { ... }` section is required to publish this package".to_string(),
+            pos: manifest_pair.clone().into_span().end_pos(),
+        }
+    )?;
+
+    let object_pair = find(metadata_section_pair, Rule::object);
+    check_object_fields(object_pair.clone(), &[
+        "name",
+        "version",
+
+        "description",
+        "keywords",
+        "homepage",
+        "bugs",
+        "repository",
+
+        "license",
+        "license_file",
+    ])?;
+
+    let name = {
+        let (name_string, name_pair)
+            = get_string(get_field(object_pair.clone(), "name")?)?;
+        PackageName::from_str(&name_string).ok_or_else(||
+            pest::Error::CustomErrorSpan {
+                message: "Invalid package name".to_string(),
+                span: name_pair.clone().into_span(),
+            }
+        )?
+    };
+
+    let version = {
+        let (version_string, version_pair)
+            = get_string(get_field(object_pair.clone(), "version")?)?;
+        Version::from_str(&version_string).ok_or_else(||
+            pest::Error::CustomErrorSpan {
+                message: "Invalid version number".to_string(),
+                span: version_pair.clone().into_span(),
+            }
+        )?
+    };
+
+    let (description, description_pair)
+        = get_optional_string_field(object_pair.clone(), "description")?;
+    let (homepage, homepage_pair)
+        = get_optional_string_field(object_pair.clone(), "homepage")?;
+    let (bugs, bugs_pair)
+        = get_optional_string_field(object_pair.clone(), "bugs")?;
+
+    let authors = get_optional_list_field(object_pair.clone(), "authors")?
+        .into_iter().map(|(s, s_pair)| s).collect();
+    let keywords = get_optional_list_field(object_pair.clone(), "keywords")?
+        .into_iter().map(|(s, s_pair)| s).collect();
+
+    let release = Release {
+        name: name,
+        version: version,
+
+        dependencies: dependencies,
+
+        authors: authors,
+        description: description,
+        homepage: homepage,
+        bugs: bugs,
+        keywords: keywords,
+
+        license: None,
+        license_files: vec![],
+
+        readme_contents: "README contents go here".to_string(),
+        files: vec![],
+    };
+
+    Ok(release)
 }
 
 // Check that there are no unexpected or duplicate fields.
@@ -163,10 +220,40 @@ fn get_optional_field<'a>(
     None
 }
 
-fn get_string<'a>(value_pair: Pair<'a>) -> Result<(Pair<'a>, String), Error<'a>> {
+fn get_optional_string_field<'a>(
+    object_pair: Pair<'a>, field_name: &'static str)
+    -> Result<(Option<String>, Option<Pair<'a>>), Error<'a>>
+{
+    // option.map would be more concise, but closures break the "?"
+    // operator.
+    if let Some(field_value_pair) = get_optional_field(object_pair, field_name) {
+        let (field_string, field_pair) = get_string(field_value_pair)?;
+        Ok((Some(field_string), Some(field_pair)))
+    } else {
+        Ok((None, None))
+    }
+}
+
+fn get_optional_list_field<'a>(
+    object_pair: Pair<'a>, field_name: &'static str)
+    -> Result<Vec<(String, Pair<'a>)>, Error<'a>>
+{
+    let mut string_pair_tuples = vec![];
+    if let Some(list_value_pair) = get_optional_field(object_pair.clone(), field_name) {
+        let (list, list_pair) = get_list(list_value_pair)?;
+        for item_value_pair in list {
+            let (string, pair) = get_string(item_value_pair)?;
+            string_pair_tuples.push((string, pair));
+        }
+    }
+    Ok(string_pair_tuples)
+}
+
+
+fn get_string<'a>(value_pair: Pair<'a>) -> Result<(String, Pair<'a>), Error<'a>> {
     for string_value_pair in children(value_pair.clone(), Rule::string_value) {
         let s = parse_string(string_value_pair.clone())?;
-        return Ok((string_value_pair, s));
+        return Ok((s, string_value_pair));
     }
     Err(pest::Error::CustomErrorSpan {
         message: "Expected string".to_string(),
@@ -174,10 +261,10 @@ fn get_string<'a>(value_pair: Pair<'a>) -> Result<(Pair<'a>, String), Error<'a>>
     })
 }
 
-fn get_list<'a>(value_pair: Pair<'a>) -> Result<(Pair<'a>, Vec<Pair<'a>>), Error<'a>> {
+fn get_list<'a>(value_pair: Pair<'a>) -> Result<(Vec<Pair<'a>>, Pair<'a>), Error<'a>> {
     for list_value_pair in children(value_pair.clone(), Rule::list_value) {
         let v = children(list_value_pair.clone(), Rule::value);
-        return Ok((list_value_pair, v));
+        return Ok((v, list_value_pair));
     }
     Err(pest::Error::CustomErrorSpan {
         message: "Expected list".to_string(),
@@ -306,14 +393,22 @@ fn find_in_pairs<'a>(mut pairs: Pairs<'a>, rule: pest_parser::Rule) -> Pair<'a> 
 
 
 pub fn test_parser() {
-    let pairs = ManifestParser::parse_str(Rule::manifest_eof, " \n pm 1.0 // yay \n\n\ndependencies { \njs/left-pad: ^1.2.3 // foo\n}").unwrap_or_else(|e| panic!("{}", e));
-    print_pairs(pairs, 0);
+    // let pairs = ManifestParser::parse_str(Rule::manifest_eof, " \n pm 1.0 // yay \n\n\ndependencies { \njs/left-pad: ^1.2.3 // foo\n}").unwrap_or_else(|e| panic!("{}", e));
+    // print_pairs(pairs, 0);
 
-    println!("dependencies: {:?}", get_dependencies("pm 1.0\ndependencies { \njs/left-pad: ^1.2.3 // foo\n js/right-pad: >=4.5.6 <5.0.0 // foo\n}").unwrap_or_else(|e| panic!("{}", e)));
-    // println!("dependencies: {:?}", get_dependencies("pm 1.0 \n\ndependencies { \njs/left-pad: ^1.2.3 // foo\n js/right-pad: >=4.x.6 <5.0.0 // foo\n}").unwrap_or_else(|e| panic!("{}", e)));
-    // println!("dependencies: {:?}", get_dependencies("pm 1.0 \n\ndependencies { \njs/left-pad: ^1.2.3 // foo\n js/left-pad: >=4.5.6 <5.0.0 // foo\n}").unwrap_or_else(|e| panic!("{}", e)));
-    // println!("dependencies: {:?}", get_dependencies("pm 1.0 \n\ndependencies { \n}\ndependencies{\n}").unwrap_or_else(|e| panic!("{}", e)));
-    // println!("dependencies: {:?}", get_dependencies("pm 1.0 \npackage { \n}\npackage{\n}").unwrap_or_else(|e| panic!("{}", e)));
+    // println!("dependencies: {:?}", parse_into_dependencies("pm 1.0\ndependencies { \njs/left-pad: ^1.2.3 // foo\n js/right-pad: >=4.5.6 <5.0.0 // foo\n}").unwrap_or_else(|e| panic!("{}", e)));
+    println!("release: {:?}", parse_into_release(r#"
+        pm 1.0
+        dependencies {
+            js/left-pad: ^1.2.3 // foo
+            // bar
+            js/right-pad: >=4.5.6 <5.0.0
+        }
+        package {
+            name: "js/foo"
+            version: "0.0.0"
+        }
+    "#).unwrap_or_else(|e| panic!("{}", e)));
 }
 
 fn print_pairs<'a>(pairs: pest::iterators::Pairs<pest_parser::Rule, pest::inputs::StrInput<'a>>, indent: usize) {
