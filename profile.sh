255a2df25e3189c15b9d46a8f3242403abaf58c0
Jo Liss <joliss42@gmail.com>
2017-10-10 18:07:09 +0000

    Update profile.sh to handle workspaces.

--- profile.sh
+++ profile.sh
@@ -9,15 +9,15 @@ if ! test -f "`which flamegraph.pl 2> /dev/null`"; then
 fi
 
 if test "$1" = --help -o "$1" = ""; then
-    echo "Usage: $0 args..."
+    echo "Usage: $0 <client|server|lib> args..."
     echo
     echo 'Specify arguments for the test binary, such as the name of the'
     echo 'test case or benchmark to run.'
     echo
     echo 'Examples:'
     echo
-    echo "$0 foo_test"
-    echo "$0 --bench bar_bench"
+    echo "$0 client foo_test"
+    echo "$0 lib --bench --ignored bar_bench"
     echo
     echo 'Note that if you omit the --bench argument for a benchmark, it will'
     echo 'run the inner loop only once, resulting in inaccurate profiling data.'
@@ -64,15 +64,28 @@ function restore_kptr_restrict() {
 
 set -e
 
+dir="$1" # client, server or lib
+shift
+if [ "$dir" = lib ]; then
+    binary="pm_lib"
+elif [ "$dir" = server ]; then
+    binary="pm_server"
+else # client
+    binary="pm"
+fi
+
+cd "$dir"
+
 cargo test --release --no-run
-sudo perf record -g -- "`ls -tr target/release/package_manager-* | grep -v \\.d$ | tail -n 1`" --test-threads 1 "$@"
+
+sudo perf record -g -- "$(ls -tr ../target/release/"$binary"-* | grep -v \\.d$ | tail -n 1)" --test-threads 1 "$@"
 sudo chmod 644 perf.data
 disable_kptr_restrict
 perf script > out.perf
 restore_kptr_restrict
 stackcollapse-perf.pl < out.perf > out.folded
 rust_demangle < out.folded > out.folded.demangled
-flamegraph.pl < out.folded.demangled > flamegraph.svg
+flamegraph.pl < out.folded.demangled > ../flamegraph.svg
 rm -f perf.data{,.old} out.perf out.folded{,.demangled}
 echo
 echo 'Output placed in flamegraph.svg'
