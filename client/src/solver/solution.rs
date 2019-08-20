1048f694779ba717d2041ff8769ab8f74225c4b4
Jo Liss <joliss42@gmail.com>
2019-08-20 16:20:16 +0000

    Remove another FromIterator impl.

--- client/src/solver/solution.rs
+++ client/src/solver/solution.rs
@@ -39,28 +39,16 @@ impl Mappable for PartialSolution {
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Solution(pub BTreeMap<PackageName, Version>);
 
-impl FromIterator<(Arc<PackageName>, Arc<Version>)> for Solution {
-    fn from_iter<T>(iter: T) -> Solution
-    where
-        T: IntoIterator<Item = (Arc<PackageName>, Arc<Version>)>,
-    {
-        let iter_of_owned = iter.into_iter().map(|(package_name, version)| {
-            let pn = Arc::try_unwrap(package_name).unwrap_or_else(|p| (*p).clone());
-            let ver = Arc::try_unwrap(version).unwrap_or_else(|v| (*v).clone());
-            (pn, ver)
-        });
-        Solution(BTreeMap::<PackageName, Version>::from_iter(iter_of_owned))
-    }
-}
-
 impl From<PartialSolution> for Solution {
     fn from(partial_solution: PartialSolution) -> Solution {
         // Strip all paths from a PartialSolution to obtain a Solution
-        partial_solution
-            .iter()
-            .map(|(package_name, justified_version)| {
-                (package_name.clone(), justified_version.version.clone())
-            })
-            .collect()
+        let sln: BTreeMap<PackageName, Version> =
+            partial_solution
+                .iter()
+                .map(|(package_name, justified_version)| {
+                    ((**package_name).clone(), (*justified_version.version).clone())
+                })
+                .collect();
+        Solution(sln)
     }
 }
